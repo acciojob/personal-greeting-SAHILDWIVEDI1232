cb6e3d96f0980b725117337b1c5cada3fdd3f5f2
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-02-02 13:51:28 +0530

    Refactor App component for cleaner syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 
 import React from "react";
-import './../styles/App.css';
 import PersonalizedGreeting from "./PersonalizedGreeting";
+
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    <PersonalizedGreeting/>
+      <PersonalizedGreeting />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
